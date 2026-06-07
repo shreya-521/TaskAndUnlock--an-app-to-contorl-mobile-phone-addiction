0e82a34cbdffbf643a788503d3c6943f74a0afe8
shreya-521 <shreyaanadkumar3@gmail.com>
2026-06-07 10:29:36 +0530

    Inject dynamic namespaces into subprojects to fix AGP 8.0+ build crash

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,26 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        val android = extensions.findByName("android")
+        if (android != null) {
+            try {
+                val getNamespaceMethod = android.javaClass.getMethod("getNamespace")
+                val namespace = getNamespaceMethod.invoke(android)
+                if (namespace == null) {
+                    val setNamespaceMethod = android.javaClass.getMethod("setNamespace", String::class.java)
+                    val fallbackNamespace = "com.taskandunlock." + project.name.replace(":", "_").replace("-", "_")
+                    setNamespaceMethod.invoke(android, fallbackNamespace)
+                    logger.quiet("Dynamically injected namespace for subproject ${project.name} -> $fallbackNamespace")
+                }
+            } catch (e: Exception) {
+                // Ignore if getNamespace/setNamespace does not exist on older AGP versions
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
